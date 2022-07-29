8e68c925eb274ab14166fb292598695d6f9c4f68
unknown <teodosius77@gmail.com>
2022-07-29 10:32:51 +0300

    Add solution

---
+++ GenericsApp/Generics.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsApp
+{
+    public class GenericStack<T>
+    {
+        private readonly T[] _items;
+        private int _currentIndex = -1;
+        private readonly int _maxSize;
+
+        public GenericStack(int maxSize = 10)
+        {
+            _maxSize = maxSize;
+            _items = new T[maxSize];
+        }
+        public int Count => _currentIndex + 1;
+        public void SetItemAtIndex(T item, int index)
+        {
+
+            try
+            {
+                _items[index] = item;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Enter a  position smaller than 10");
+            }
+        }
+
+        public T GetItemByIndex(int index)
+        {
+            try
+            {
+
+                return _items[index];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
+            }
+
+        }
+        public T copy;
+        public void SwapItemsByIndex(int index1, int index2)
+        {
+            copy = _items[index1];
+            _items[index1] = _items[index2];
+            _items[index2] = copy;
+
+        }
+       
+     
+    }
+}
---
+++ GenericsApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using GenericsApp;
+
+
+var intStack = new GenericStack<int>();
+intStack.SetItemAtIndex(7,11);
+intStack.SetItemAtIndex(8,3);
+intStack.SetItemAtIndex(9,6);
+intStack.SetItemAtIndex(10,8);
+intStack.SwapItemsByIndex(2, 8);
+
+for (int i=0; i<=9; i++)
+{
+    Console.WriteLine(intStack.GetItemByIndex(i));
+}
+
+// In the scenario where we dont use a for, we want to get an individual item 
+//we need to maek sure that the searched item is in the range of the array
+try
+{
+    Console.WriteLine(intStack.GetItemByIndex(11));
+}
+catch
+{
+    Console.WriteLine("Index too big");
+}
+
