2d4ecfa07fdf5e3348281a3938a642f4fe87dcd6
unknown <teodosius77@gmail.com>
2022-08-01 09:20:18 +0300

    Add solution

--- GenericsApp/Generics.cs
+++ GenericsApp/Generics.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GenericsApp
 {
-    public class GenericStack<T>
+    public class GenericStack<T> where T : IComparable
     {
         private readonly T[] _items;
         private int _currentIndex = -1;
@@ -27,7 +27,7 @@ namespace GenericsApp
             }
             catch (IndexOutOfRangeException)
             {
-                Console.WriteLine("Enter a  position smaller than 10");
+                Console.WriteLine("Enter an index smaller than 10");
             }
         }
 
@@ -46,13 +46,81 @@ namespace GenericsApp
         }
         public T copy;
         public void SwapItemsByIndex(int index1, int index2)
+
         {
-            copy = _items[index1];
-            _items[index1] = _items[index2];
-            _items[index2] = copy;
+            try
+            {
+                copy = _items[index1];
+                _items[index1] = _items[index2];
+                _items[index2] = copy;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Enter an index smaller than 10");
+            }
+        }
+
+        public int count1 = 0;
+        public int count2 = 0;
+        public T copy1;
+        public T copy2;
+        public void SwapItemsByValue(T value1, T value2)
+        {
+            foreach (T item in _items)
+            {
+                if (item.CompareTo(value1) == 0)
+                {
+                    
+                    copy1 = item;
+                    foreach (T element in _items)
+                    {
+                        if (element.CompareTo(value2) == 0)
+                        {
+                            
+                            copy2 = element;
+                            break;
+                        }
+                        else count2++;
+                    }
+                    break;
+                }
+                else count1++;
+            }
+           
+                copy = _items[count1];
+                _items[count1] = _items[count2];
+                _items[count2] = copy;
+            
+          
+        }
+
+        public int countindex = 0;
+
+        public void SwapItems(int index, T value)
+        {
+            foreach (T item in _items)
+            {
+                if (item.CompareTo(value) == 0)
+                {
+                    break;
+                }
+                else countindex++;
+            }
+            try
+            {
+                copy = _items[index];
+                _items[index] = _items[countindex];
+                _items[countindex] = copy;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Enter an index smaller than 10");
+            }
+
 
         }
-       
-     
     }
+
+
 }
+
--- GenericsApp/Program.cs
+++ GenericsApp/Program.cs
@@ -2,18 +2,20 @@
 
 
 var intStack = new GenericStack<int>();
-intStack.SetItemAtIndex(7,11);
+intStack.SetItemAtIndex(7,2);
 intStack.SetItemAtIndex(8,3);
 intStack.SetItemAtIndex(9,6);
 intStack.SetItemAtIndex(10,8);
-intStack.SwapItemsByIndex(2, 8);
+intStack.SwapItemsByIndex(10, 8);
+intStack.SwapItemsByValue(7, 9);
+intStack.SwapItems(2, 10);
 
-for (int i=0; i<=9; i++)
+for (int i = 0; i <= 9; i++)
 {
     Console.WriteLine(intStack.GetItemByIndex(i));
 }
 
-// In the scenario where we dont use a for, we want to get an individual item 
+/*// In the scenario where we dont use a for, we want to get an individual item 
 //we need to maek sure that the searched item is in the range of the array
 try
 {
@@ -22,5 +24,5 @@ try
 catch
 {
     Console.WriteLine("Index too big");
-}
+}*/
 
